d501251ad99a84cc975c9152841ef60f18a2c603
Sarwar Murshed Shatil <38027343+shatilsarower@users.noreply.github.com>
2022-09-16 17:36:41 +0600

    making transparent appbar

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        useMaterial3: true,
       ),
       home: const HomePage(),
     );
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,6 +6,12 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      backgroundColor: CupertinoColors.darkBackgroundGray,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
+    );
   }
 }
