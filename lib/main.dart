ed459d942779ffd95eaaa5f31abf3af353455291
Sarwar Murshed Shatil <38027343+shatilsarower@users.noreply.github.com>
2022-09-16 17:19:12 +0600

    Doing refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_homepage/homepage.dart';
+import 'package:google_homepage/pages/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,12 +8,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
